1a7e2f3c82cc0e999f65a3626f896b6dbae0ee7f
amir motavas <amir.mts79@gmail.com>
2022-06-04 15:58:22 +0430

    master

--- Generic/target/rls/debug/deps/Generic-0f76d562adb6150c.d
+++
@@ -1,5 +0,0 @@
-/home/amir/Documents/Learn_Rust3/Generic/target/rls/debug/deps/Generic-0f76d562adb6150c.rmeta: src/main.rs
-
-/home/amir/Documents/Learn_Rust3/Generic/target/rls/debug/deps/Generic-0f76d562adb6150c.d: src/main.rs
-
-src/main.rs:
