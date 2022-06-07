2e1e7ceca2919f78689e659ba268f773ecf99e70
amir motavas <amir.mts79@gmail.com>
2022-06-07 13:19:27 +0430

    Another example for Result

--- option_and_result/src/main.rs
+++ option_and_result/src/main.rs
@@ -117,22 +117,85 @@
 
 // }
 // -----------------------------------------------------------------------------
-fn check_if_five(number: i32) -> Result<i32, String> {
-    match number {
-        5 => Ok(number),
-        _ => Err("Sorry, the number wasn't five.".to_string()), // This is our error message
+// fn check_if_five(number: i32) -> Result<i32, String> {
+//     match number {
+//         5 => Ok(number),
+//         _ => Err("Sorry, the number wasn't five.".to_string()), // This is our error message
+//     }
+// }
+
+// fn main() {
+//     let mut result_vec = Vec::new(); // Create a new vec for the results
+
+//     for number in 2..7 {
+//         result_vec.push(check_if_five(number)); // push each result into the vec
+//     }
+
+//     println!("{:?}", result_vec);
+// }
+
+// -------------------------------------------------------------------------------------
+// Another Example
+
+mod checked {
+    // Mathematical "errors" we want to catch
+    #[derive(Debug)]
+    pub enum MathError {
+        DivisionByZero,
+        NonPositiveLogarithm,
+        NegativeSquareRoot,
+    }
+
+    pub type MathResult = Result<f64, MathError>;
+
+    pub fn div(x: f64, y: f64) -> MathResult {
+        if y == 0.0 {
+            // This operation would `fail`, instead let's return the reason of
+            // the failure wrapped in `Err`
+            Err(MathError::DivisionByZero)
+        } else {
+            // This operation is valid, return the result wrapped in `Ok`
+            Ok(x / y)
+        }
+    }
+
+    pub fn sqrt(x: f64) -> MathResult {
+        if x < 0.0 {
+            Err(MathError::NegativeSquareRoot)
+        } else {
+            Ok(x.sqrt())
+        }
+    }
+
+    pub fn ln(x: f64) -> MathResult {
+        if x <= 0.0 {
+            Err(MathError::NonPositiveLogarithm)
+        } else {
+            Ok(x.ln())
+        }
+    }
+}
+
+// `op(x, y)` === `sqrt(ln(x / y))`
+fn op(x: f64, y: f64) -> f64 {
+    // This is a three level match pyramid!
+    match checked::div(x, y) {
+        Err(why) => panic!("{:?}", why),
+        Ok(ratio) => match checked::ln(ratio) {
+            Err(why) => panic!("{:?}", why),
+            Ok(ln) => match checked::sqrt(ln) {
+                Err(why) => panic!("{:?}", why),
+                Ok(sqrt) => sqrt,
+            },
+        },
     }
 }
 
 fn main() {
-    let mut result_vec = Vec::new(); // Create a new vec for the results
-
-    for number in 2..7 {
-        result_vec.push(check_if_five(number)); // push each result into the vec
-    }
-
-    println!("{:?}", result_vec);
+    // Will this fail?
+    println!("{}", op(1.0, 10.0));
 }
+
 
 
 
